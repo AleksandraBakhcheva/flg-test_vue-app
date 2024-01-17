d094f922cb80869586b36cf3b369ed50974e991b
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-01-18 01:03:22 +0300

    vue deploy files update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:AleksandraBakhcheva/flg-test_vue-app.git master:gh-pages
+# git push -f git@github.com:aleksandrabakhcheva/flg-test_vue-app.git master:gh-pages
 
 cd -
